681e003c97d4da685a8576971195346c707558e4
liushiquan <1012434131@qq.com>
2017-09-28 22:46:47 +0800

    first comit

---
+++ TestPlugin/testplugin.h
@@ -0,0 +1,13 @@
+#ifndef TESTPLUGIN_H
+#define TESTPLUGIN_H
+
+#include "testplugin_global.h"
+
+class TESTPLUGINSHARED_EXPORT TestPlugin
+{
+
+public:
+    TestPlugin();
+};
+
+#endif // TESTPLUGIN_H
